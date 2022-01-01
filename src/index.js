10a4c23eb99bd2f233e691215763688df3420406
pokolga <pokolga1801@gmail.com>
2022-01-01 21:50:21 +0300

    init: task reverse integer

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    n = Math.abs(n);
+    const arr = String(n).split("");
+    arr.reverse();
+    return Number(arr.join(""));
 }
